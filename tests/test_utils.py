14075fe709cb10cf2995350ccf6b3a776b8b1bb7
Surkal <user@server.fake>
2018-09-06 19:26:06 +0200

    changed the type of a parameter, for more explicitness

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,18 +10,19 @@ class TestLangList(TestCase):
         self.text = extract_text('/src/tests/page/gå_before.txt')
 
     def test_no_text(self):
-        assert page_languages('', 'set') == set({})
-        assert page_languages('', 'list') == list()
+        assert page_languages('', set) == set({})
+        assert page_languages('', list) == list()
         assert page_languages('', 'toto') == None
 
     def test_no_type(self):
+        assert page_languages(self.text, str) == None
         assert page_languages(self.text, 'toto') == None
         assert page_languages(self.text, typ='toto') == None
 
     def test_correct_return_type(self):
-        assert isinstance(page_languages(self.text, 'set'), set)
-        assert isinstance(page_languages(self.text, 'list'), list)
+        assert isinstance(page_languages(self.text, set), set)
+        assert isinstance(page_languages(self.text, list), list)
 
     def test_success(self):
-        assert page_languages(self.text, 'set') == set({'da', 'no', 'sv'})
-        assert page_languages(self.text, 'list') == ['da', 'no', 'sv']
+        assert page_languages(self.text, set) == set({'da', 'no', 'sv'})
+        assert page_languages(self.text, list) == ['da', 'no', 'sv']
--- utils.py
+++ utils.py
@@ -5,11 +5,11 @@ def page_languages(text, typ):
     Returns all the languages of the page, in the form of the requested type.
     It only works for the french witionary and only accept type 'list' or 'set'.
     """
-    if not typ in ('list', 'set'):
+    if not isinstance(typ, type):
         return None
-    if typ == 'set':
-        return set(re.findall(r"{{langue\|(?P<lang>\w+)}\}", text))
-    return list(re.findall(r"{{langue\|(?P<lang>\w+)}\}", text))
+    if not any(typ.__name__ == x for x in ('list', 'set')):
+        return None
+    return typ((re.findall(r"{{langue\|(?P<lang>\w+)}\}", text)))
 
 def extract_text(filename):
     with open(filename, 'r') as f:
